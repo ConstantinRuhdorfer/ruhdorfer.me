e5f099bd197bb2efd1015403dfcc6f426d2e7d2e
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2021-08-28 09:23:01 +0200

    First layout

---
+++ src/menu/Menu.tsx
@@ -0,0 +1,11 @@
+import './Menu.css'
+
+function Menu() {
+    return (
+        <div className='item-1 Menu'>
+            <p>I am some serious Menu</p>
+        </div>
+    )
+}
+
+export default Menu;
